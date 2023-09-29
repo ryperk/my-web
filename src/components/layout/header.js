46c317a025c0b59f23a89c0b731a07dbd54ba322
Ryan Permana <ryan.permana@adira.co.id>
2023-09-29 08:41:15 +0700

    change work to blog

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -7,7 +7,7 @@ export default function Header() {
         {/* <h1 className="font-bold text-5xl text-gray-100 font-serif">R</h1> */}
         <div className="flex flex-row gap-10">
           <Link href="/" className=" text-gray-400 font-sans active:text-gray-100">home</Link>
-          <Link href="/works" className=" text-gray-400 font-sans">works</Link>
+          <Link href="/blog" className=" text-gray-400 font-sans">blog</Link>
           <Link href="/contact" className=" text-gray-400 font-sans">contact</Link>
         </div>
       </div>
